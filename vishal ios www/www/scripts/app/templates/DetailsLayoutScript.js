052b17c9dd141732d5e5e10fba1b3aedfc73e618
AmaHack <amadeus.hackathon@gmail.com>
2013-11-10 01:02:18 +0530

    details page

--- vishal ios www/www/scripts/app/templates/DetailsLayoutScript.js
+++ vishal ios www/www/scripts/app/templates/DetailsLayoutScript.js
@@ -1,20 +1,53 @@
 Aria.tplScriptDefinition({
     $classpath : "app.templates.DetailsLayoutScript",
 	$constructor: function(){
+	
 	},    
 	$prototype : {
 		$dataReady: function() {
-			var myScroll;
-			console.log(this.data);
+			console.log('Details Layout');
 		},
 		
-		$displayReady: function(){
-			myScroll = new iScroll('wrapper');
+		$displayReady: function() {
+			
+			var detTpl = this;
+			
+			// create iscroll
+			detTpl.data.myScroll = new iScroll('wrapper');
 			document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+			
+			// refresh iscroll whenever change in orientation
 			$(window).bind('orientationchange', function() {
-				myScroll.refresh();
+				detTpl.data.myScroll.refresh();
 			});
+			
+			// reset UI
+			detTpl.hidePopup();
+			detTpl.packageClicked();
+		},
+		
+		showPopup: function(events, args) {
+			$(".popup2, .mask").show();
+		},
+		
+		hidePopup: function(events, args) {
+			$(".popup2, .mask").hide();
+		},
+		
+		eventClicked: function(events, args) {
+			$("#eventslist").show();
+			$("#packageList").hide();
+			$("#packages").removeClass("selected");
+			$("#events").addClass("selected");
+			this.data.myScroll.refresh()
+		},
+		
+		packageClicked: function(events, args) {
+			$("#eventslist").hide();
+			$("#packageList").show();
+			$("#packages").addClass("selected");
+			$("#events").removeClass("selected");
+			this.data.myScroll.refresh();
 		}
-
  	}
 })
